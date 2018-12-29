99b056ea3c21b19519382d51a20a3428295db13b
vladislawovk <vladislawovk@gmail.com>
2018-12-29 15:27:38 +0300

    Understanding Strings And Finishing Up Primitive Data Types 3-20

---
+++ Strings/src/com/vladislawovk/Main.java
@@ -0,0 +1,26 @@
+package com.vladislawovk;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String myString = "This is a string";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + " \u00A9 2018";
+        System.out.println("myString is equal to " + myString);
+
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println("The result is " + numberString);
+
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("lastString is equal to " + lastString);
+
+        double doubleNumber = 120.47;
+        lastString = lastString + doubleNumber;
+        System.out.println("lastString value: " + lastString);
+    }
+}
