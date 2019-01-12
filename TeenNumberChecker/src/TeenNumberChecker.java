82fad3e1d2ef2d7aded9d62cf82644fba95589fe
vladislawovk <vladislawovk@gmail.com>
2019-01-12 16:10:54 +0300

    Coding Exercise 6: Teen Number Checker

---
+++ TeenNumberChecker/src/TeenNumberChecker.java
@@ -0,0 +1,8 @@
+public class TeenNumberChecker {
+    public static void main(String[] args) {
+
+    }
+    public static boolean hasTeen(int val1, int val2, int val3) {
+        return((val1 >= 13 && val1 <=19) || (val2 >= 13 && val2 <=19) || (val3 >= 13 && val3 <=19));
+    }
+}
