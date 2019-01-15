8241f497e71e12bd896891c7daf6f5cf25a81c2e
vladislawovk <vladislawovk@gmail.com>
2019-01-15 14:32:44 +0300

    Coding Exercise 10: Playing Cat

---
+++ PlayingCat/src/PlayingCat.java
@@ -0,0 +1,21 @@
+public class PlayingCat {
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if(!summer && (temperature >= 25) && (temperature <= 35)) {
+            return true;
+        } else if(summer && (temperature >= 25) && (temperature <= 45)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+// 2 вариант решения
+//    public static boolean isCatPlaying(boolean summer, int temperature) {
+//
+//        if ((temperature >= 25 && temperature <= 35) || (summer && (temperature >= 25 && temperature <= 45))) {
+//            return true;
+//        } else
+//            return false;
+//    }
+}
