02c7593545da433bc30845d232e4d82b87f21e70
vladislawovk <vladislawovk@gmail.com>
2019-03-11 13:29:29 +0300

    Coding Exercise: Point

---
+++ Point/src/com/vladislawovk/Point.java
@@ -0,0 +1,44 @@
+package com.vladislawovk;
+
+public class Point {
+
+    private int x;
+    private int y;
+
+    public Point() {
+
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public double distance() {
+        return distance(0,0);
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt(Math.pow(x - getX(),2) + Math.pow(y - getY(),2));
+    }
+
+    public double distance(Point b) {
+        return distance(b.x, b.y);
+    }
+}
