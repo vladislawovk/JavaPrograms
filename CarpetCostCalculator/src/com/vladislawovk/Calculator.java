e8374d12ef316fa55ffc30982bca649e81858a0e
vladislawovk <vladislawovk@gmail.com>
2019-03-11 14:52:43 +0300

    Coding Exercise: Carpet Cost Calculator

---
+++ CarpetCostCalculator/src/com/vladislawovk/Calculator.java
@@ -0,0 +1,16 @@
+package com.vladislawovk;
+
+public class Calculator {
+
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost() {
+        return carpet.getCost() * floor.getArea();
+    }
+}
